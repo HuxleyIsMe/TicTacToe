984b264f662a5a85e85d6691e9930361f9315bb7
HuxleyIsMe <huxley.millard@gmail.com>
2025-04-25 14:26:45 +0100

    style: split up the stylesheets

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import "./style.css";
+import "./components/Board/board.css";
+import "./components/commentator.css";
 import { Board } from "./components/Board/board.js";
 
 const ticTacToe = new Board("#app");
