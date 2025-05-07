b14096bf7fb2a3aa63264c63466928228e06a9bc
hux <huxley.millard@gmail.com>
2025-05-07 19:17:22 +0200

    feat: should only mount this component once

--- src/components/Opponent/opponent.ts
+++ src/components/Opponent/opponent.ts
@@ -3,35 +3,40 @@ import type { UIHandlerReturnType } from "../../sharedUtils/UIHelper";
 import type {Player} from "../../types"
 
 export class Opponent {
+    isAlreadyMounted: boolean;
     isPlaying: boolean;
     playsAs: Player;
     root: string;
     UIHandler: UIHandlerReturnType;
 
     constructor(root: string) {
+        this.isAlreadyMounted = false;
         this.isPlaying = false;
         this.playsAs = 'O';
         this.root = root;
-        this.UIHandler = UIHandler();
-        
+        this.UIHandler = UIHandler();   
     }
 
     onStart() {
-        if(document.getElementById(this.root) == null || !this.root){
-            console.error('No root provided to attach opponent to!');
-            return
-        }
-        document.getElementById(this.root)!.innerHTML = `<div class="form-check form-switch">
-            <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-            <label class="form-check-label mt-1" for="flexSwitchCheckDefault">Play computer?</label>
-        </div>`
+        if(!this.isAlreadyMounted) {
+            if(document.getElementById(this.root) == null || !this.root){
+                console.error('No root provided to attach opponent to!');
+                return
+            }
+            document.getElementById(this.root)!.innerHTML = `<div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                <label class="form-check-label mt-1" for="flexSwitchCheckDefault">Play computer?</label>
+            </div>`
 
-        document
-        .querySelector(`#flexSwitchCheckDefault`)!
-        .addEventListener(
-          "change",
-          () => this.isPlaying = !this.isPlaying,
-        );
+            document
+            .querySelector(`#flexSwitchCheckDefault`)!
+            .addEventListener(
+            "change",
+            () => this.isPlaying = !this.isPlaying,
+            );
+
+            this.isAlreadyMounted = true
+        }
     }
 
     onNextTurn({gameState, cellIDs, turn}) {
